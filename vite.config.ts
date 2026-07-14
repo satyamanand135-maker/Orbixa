ae50a308eb087dc8dc26ae54cabe9a9273684ab2
Satyam Anand <satyamanand135@gmail.com>
2026-07-14 21:07:53 +0530

    Allow Railway Host

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
@@ -17,6 +17,10 @@ export default defineConfig(() => {
       hmr: process.env.DISABLE_HMR !== 'true',
       // Disable file watching when DISABLE_HMR is true to save CPU during agent edits.
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
+
+      allowedHosts: [
+        "orbixa-production.up.railway.app"
+      ]
     },
   };
 });
